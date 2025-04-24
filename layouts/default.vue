64a104e7fd14d357d7afb3312ca1fe1711988725
xunita <yayasopeguesoro@gmail.com>
2025-04-24 23:07:13 +0200

    minor changes

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <!-- eslint-disable vue/html-self-closing -->
 <template>
   <div class="w-full h-full">
-    <div class="jmai-app-default-layout lg:px-48 sm:px-24 max-sm:px-10 h-full">
+    <div class="jmai-app-default-layout lg:px-80 md:px-32 sm:px-24 max-sm:px-10 h-full">
       <HeaderBase />
       <div class="jmai-app-default-layout-content h-full relative z-10">
         <slot></slot>
